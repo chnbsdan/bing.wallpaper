405974d59d7013c32777a1f2cef1bc505b18e853
chnbsdan <chnbsdan@gmail.com>
2026-08-09 21:57:36 +0800

    Update images.ts

--- composables/images.ts
+++ composables/images.ts
@@ -1,47 +1,17 @@
-import type { BingImageMeta } from '~/types'
-
-const state = reactive({
-  hasMore: true,
-  isFetching: false,
-  imageMap: new Map<string, BingImageMeta>(),
-})
-
-async function loadImages(query: { idx: number, count: number, mkt: string }) {
-  if (state.isFetching || !state.hasMore)
-    return
-
-  state.isFetching = true
-  const images = await $fetch('/api/images', { query })
-  state.isFetching = false
-  state.hasMore = images.length >= query.count - 2
-  images.forEach(image => state.imageMap.set(image.date, image))
+interface ImagesQuery {
+  mkt: string
+  idx: number
+  count: number
 }
 
-function resetImages() {
-  state.imageMap = new Map()
-  state.hasMore = true
-  state.isFetching = false
-}
+export default defineEventHandler(
+  async (event) => {
+    const query = getQuery<ImagesQuery>(event)
 
-async function getImageByKey(date: string, mkt: string) {
-  if (!date)
-    return null
+    const idx = Number(query.idx) || 0
+    const count = Number(query.count) || 1
+    const market = useValidMarket(event)
 
-  if (state.imageMap.has(date)) {
-    return state.imageMap.get(date)!
-  }
-  else {
-    try {
-      const image = await $fetch('/api/image', { query: { date, mkt } })
-      state.imageMap.set(date, image)
-      return image
-    }
-    catch {
-      return null
-    }
-  }
-}
-
-export function useImages() {
-  return { ...toRefs(state), loadImages, resetImages, getImageByKey }
-}
+    return await getCachedImagesFromStorage(idx, count, market)
+  },
+)
