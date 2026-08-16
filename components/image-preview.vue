7ec668bc33648d7a741203b2f04f612c8a26747c
chnbsdan <chnbsdan@gmail.com>
2026-08-16 10:27:17 +0800

    Update image-preview.vue

--- components/image-preview.vue
+++ components/image-preview.vue
@@ -24,7 +24,7 @@ const previewDatePrev = computed(() => {
   const c = new Date(previewDate.value)
   const d = new Date(c.setDate(c.getDate() - 1))
 
-  if (d < new Date('2016-03-05'))
+  if (d < new Date('2016-02-01'))
     return ''
 
   return formatDate(d, 'YYYY-MM-DD')
